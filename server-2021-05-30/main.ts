cdc7f3e4fd13118168130e806f6e571e661dc051
h666t <1845720928@qq.com>
2021-06-05 00:23:59 +0800

    upload

--- server-2021-05-30/main.ts
+++ server-2021-05-30/main.ts
@@ -3,8 +3,25 @@ const server = http.createServer()
 server.addListener("request",(request: http.IncomingMessage, response: http.ServerResponse)=>{
     // console.log(request);
     // console.log(response);
-    response.write("hello, world\n")
-    response.end();
+    console.log(request.httpVersion);
+    console.log(request.url);
+    console.log(request.method);
+    console.log(request.headers);
+    
+    let chunkArray = []
+    request.on("data",(chunk)=>{
+        chunkArray.push(chunk)
+    })
+
+    request.on("end",()=>{
+        console.log(Buffer.concat(chunkArray).toString());
+        response.write("hello, world\n")
+        response.end();
+    })
+    
 });
 
-server.listen(8888);
+server.listen(8888, ()=>{
+    console.log('start to listen port 8888');
+    // console.log(server.address());
+});
