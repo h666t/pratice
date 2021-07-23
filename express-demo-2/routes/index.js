fbebbd4afef70f8e251ad77371ff0174c6a0fa9c
h666t <1845720928@qq.com>
2021-07-24 00:13:26 +0800

    upload

--- express-demo-2/routes/index.js
+++ express-demo-2/routes/index.js
@@ -6,10 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/xxx', function(req, res, next) {
-  res.render('mine/index.ejs',{title: 'mine'})
-  // res.send('xxx');
-  // res.render('index', { title: 'Express' });
-});
-
 module.exports = router;
